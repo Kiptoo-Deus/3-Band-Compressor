e78cb228057ec4ae378bdd64b6fccbdcacf579d5
Joeljoram <deusjoel150@gmail.com>
2022-11-18 11:41:55 +0300

    Save feature

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -176,12 +176,19 @@ void _3BandCompressorAudioProcessor::getStateInformation (juce::MemoryBlock& des
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+    juce::MemoryOutputStream mos (destData, true);
+    apvts.state.writeToStream(mos);
 }
 
 void _3BandCompressorAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid())
+    {
+        apvts.replaceState(tree);
+    }
 }
 juce::AudioProcessorValueTreeState::ParameterLayout _3BandCompressorAudioProcessor::createParameterLayout()
 {
